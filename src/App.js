2665884fe5883d616cd2db290a5464a1d759e633
SamHo <sam.ho@kdanmobile.com>
2020-08-28 15:46:13 +0800

    Finish fetch api and render items with bootstrap style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setUsers(json))
+      .catch(error => {
+        setHasError(true);
+      })
+      .finally(() => setIsLoading(false))
+    }
+
+    if (isLoading) {
+      fetchData();
+    }
+  }, [isLoading])
+
+  const handleClick = () => setIsLoading(true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!hasError ?
+          (<Button color="primary" onClick={handleClick} disabled={isLoading}>
+            {isLoading ? 'loading' : 'get users'}
+          </Button>) : null}
+        {
+          users.map(user => (
+            <ListGroup>
+              <ListGroupItem>{user.name}</ListGroupItem>
+            </ListGroup>
+          ))
+        }
       </header>
     </div>
   );
